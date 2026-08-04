e650e06bf322add1c76988b14c6447f7d2850ec1
rajakumar <rajakumarck28@gmail.com>
2026-08-04 15:54:34 +0530

    modified 2

--- .github/workflows/ak.sh
+++ .github/workflows/ak.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-echo "enter the username"
-read user
-cat /etc/passwd | grep $user </dev/null
-if [[ $? == 0 ]]
-then 
-echo "user is present"
-else
-echo "user is not present"
-fi
+echo "Welcome to GitHub Actions"
+date
+hostname
+whoami
